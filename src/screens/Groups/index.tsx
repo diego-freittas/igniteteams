b39830fa7cd141559274c03569e95a9423057977
Diego <tpddiego@gmail.com>
2024-06-19 15:01:33 -0300

    inclui o botao de voltar, instala biblioteca de icone

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { Container, Title } from './styles';
+import { Header } from '../../components/Header';
 
 export function Groups() {
   return (
     <Container>
-      <Title>
-        Groups
-      </Title>
+      <Header showBackButton/>
     </Container>
   );
 }
