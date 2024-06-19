edcdaaf794711e343906c5dab7201aa3db1d786a
Diego <tpddiego@gmail.com>
2024-06-19 12:00:43 -0300

    implementa o component Loading

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
 import { ThemeProvider } from "styled-components/native";
 import { Groups } from "./src/screens/Groups";
 import theme from "./src/theme";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+import { Loading } from "./src/components/Loading";
 
 
 export default function App() {
+const[fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
+
   return (
     <ThemeProvider theme={theme}>
-      < Groups/>
-      </ThemeProvider>
+      {fontsLoaded ? < Groups/> : <Loading />}
+    </ThemeProvider>
   );
 }
 
